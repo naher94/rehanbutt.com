d560b2e515d987893b5e6b63339e4760a33606c5
Rehan <me@rehanbutt.com>
2019-06-30 22:14:19 -0400

    JS updated with Comments

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ $(document).foundation();
         header = document.querySelector('.mobile-navbar-full');
     
     burger.onclick = function() {
-        header.classList.toggle('.menu-hide');
+      header.classList.toggle('.menu-hide');
+//      remove display inline-block from nav-item-container
+//      remove display none .mobile-navbar
     }
 }());
