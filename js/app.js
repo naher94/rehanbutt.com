43024e706e5715f4ce41da2c41471b1331cb58bd
Rehan <me@rehanbutt.com>
2019-07-07 10:11:45 -0400

    JS Cleaned

--- js/app.js
+++ js/app.js
@@ -1,12 +1,1 @@
 $(document).foundation();
-
-(function(){
-    var burger = document.querySelector('.menu'),
-        header = document.querySelector('.mobile-navbar-full');
-    
-    burger.onclick = function() {
-      header.classList.toggle('.menu-hide');
-//      remove display inline-block from nav-item-container
-//      remove display none .mobile-navbar
-    }
-}());
