e9ff8bc26e5a2484eefce0688b02d40557226aee
Rehan <me@rehanbutt.com>
2020-07-13 17:01:31 -0400

    Hide Eggs
    Working on #286

--- js/app.js
+++ js/app.js
@@ -36,21 +36,28 @@ function isEgg (){
 
 function setBTS(){
   // Create our stylesheet
-  var style = document.createElement('style');
-  style.innerHTML =
-    '.behind-the-scenes-container {' +
-      'display:flex;' +
-    '}' +
-    '.easter-egg-banner-container{ display: flex; }';
-  // Get the first script tag
-  var ref = document.querySelector('script');
-  // Insert our new styles before the first script tag
-  ref.parentNode.insertBefore(style, ref);
+  var elements = document.getElementsByClassName("behind-the-scenes-container");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].classList.add("force-flex");
+  }
+
+  var eggBanner = document.getElementById("egg-banner");
+  eggBanner.classList.add("force-flex");
 }
 
 function unsetBTS(){
   //This function should remove or update the above CSS of display to NONE
-  console.log("in unsetBTS");
+  var elements = document.getElementsByClassName("behind-the-scenes-container");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].classList.remove("force-flex");
+  }
+
+  var eggBanner = document.getElementById("egg-banner");
+  eggBanner.classList.remove("force-flex");
+
+  isEggVisable = false;
+  localStorage.setItem("eggKey", isEggVisable);
+
 }
 
 isEgg();
