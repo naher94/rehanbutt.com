73b8ad88d31ab45523226c2888abe2bf28f508f4
Rehan Butt <me@rehanbutt.com>
2021-04-25 12:13:01 -0400

    Moved JS around

--- js/app.js
+++ js/app.js
@@ -140,67 +140,6 @@ isBadge();
 document.addEventListener('keydown', keyHandler, false);
 
 
-//Intro Section Emoji Hover Effect
-const planeEmoji = document.getElementById("plane-emoji");
-
-planeEmoji.addEventListener("mouseover", event => {
-  console.log("Mouse in");
-	var element = document.getElementById("plane-effect");
-  element.classList.add("intro-swipe-effect");
-});
-
-planeEmoji.addEventListener("mouseout", event => {
-  console.log("Mouse out");
-	var element = document.getElementById("plane-effect");
-  element.classList.remove("intro-swipe-effect");
-});
-
-
-const worldEmoji = document.getElementById("world-emoji");
-
-worldEmoji.addEventListener("mouseover", event => {
-  console.log("Mouse in");
-	var element = document.getElementById("world-effect");
-  element.classList.add("intro-swipe-effect");
-});
-
-worldEmoji.addEventListener("mouseout", event => {
-  console.log("Mouse out");
-	var element = document.getElementById("world-effect");
-  element.classList.remove("intro-swipe-effect");
-});
-
-
-const cameraEmoji = document.getElementById("camera-emoji");
-
-cameraEmoji.addEventListener("mouseover", event => {
-  console.log("Mouse in");
-	var element = document.getElementById("camera-effect");
-  element.classList.add("intro-swipe-effect");
-});
-
-cameraEmoji.addEventListener("mouseout", event => {
-  console.log("Mouse out");
-	var element = document.getElementById("camera-effect");
-  element.classList.remove("intro-swipe-effect");
-});
-
-
-const penguinEmoji = document.getElementById("penguin-emoji");
-
-penguinEmoji.addEventListener("mouseover", event => {
-  console.log("Mouse in");
-	var element = document.getElementById("penguin-effect");
-  element.classList.add("intro-swipe-effect");
-});
-
-penguinEmoji.addEventListener("mouseout", event => {
-  console.log("Mouse out");
-	var element = document.getElementById("penguin-effect");
-  element.classList.remove("intro-swipe-effect");
-});
-//End of Intro Section Emoji Hover Effect
-
 
 
 ///////////////////////////////////////////// Happy Day Label
@@ -275,3 +214,65 @@ function getHappyDayString() {
 
 getHappyDayString();
 ///////////////////////////////////////////// End of Happy Day Label
+
+
+//Intro Section Emoji Hover Effect
+const planeEmoji = document.getElementById("plane-emoji");
+
+planeEmoji.addEventListener("mouseover", event => {
+  console.log("Mouse in");
+	var element = document.getElementById("plane-effect");
+  element.classList.add("intro-swipe-effect");
+});
+
+planeEmoji.addEventListener("mouseout", event => {
+  console.log("Mouse out");
+	var element = document.getElementById("plane-effect");
+  element.classList.remove("intro-swipe-effect");
+});
+
+
+const worldEmoji = document.getElementById("world-emoji");
+
+worldEmoji.addEventListener("mouseover", event => {
+  console.log("Mouse in");
+	var element = document.getElementById("world-effect");
+  element.classList.add("intro-swipe-effect");
+});
+
+worldEmoji.addEventListener("mouseout", event => {
+  console.log("Mouse out");
+	var element = document.getElementById("world-effect");
+  element.classList.remove("intro-swipe-effect");
+});
+
+
+const cameraEmoji = document.getElementById("camera-emoji");
+
+cameraEmoji.addEventListener("mouseover", event => {
+  console.log("Mouse in");
+	var element = document.getElementById("camera-effect");
+  element.classList.add("intro-swipe-effect");
+});
+
+cameraEmoji.addEventListener("mouseout", event => {
+  console.log("Mouse out");
+	var element = document.getElementById("camera-effect");
+  element.classList.remove("intro-swipe-effect");
+});
+
+
+const penguinEmoji = document.getElementById("penguin-emoji");
+
+penguinEmoji.addEventListener("mouseover", event => {
+  console.log("Mouse in");
+	var element = document.getElementById("penguin-effect");
+  element.classList.add("intro-swipe-effect");
+});
+
+penguinEmoji.addEventListener("mouseout", event => {
+  console.log("Mouse out");
+	var element = document.getElementById("penguin-effect");
+  element.classList.remove("intro-swipe-effect");
+});
+//End of Intro Section Emoji Hover Effect
